fb3a0e5f1606681bb5e295ba703d9552c0dcf4ef
HIMWLWDSJ <84259149+HIMWLWDSJ@users.noreply.github.com>
2023-03-24 23:57:16 +0800

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,18 @@
+
+vim.o.updatetime = 300
+-- 等待mappings
+vim.o.timeoutlen = 100
+-- split window 从下边和右边出现
+vim.o.splitbelow = true
+vim.o.splitright = true
+-- 自动补全不自动选中
+vim.g.completeopt = "menu,menuone,noselect,noinsert"
+-- 样式
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.opt.termguicolors = true
+-- 补全增强
+vim.o.wildmenu = true
+-- Dont' pass messages to |ins-completin menu|
+vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.pumheight = 10
